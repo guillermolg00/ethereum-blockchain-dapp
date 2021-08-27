474a7375e06560cb4edebb3ee9509db8d76bf913
guillermolg <guillermolopezg00@gmail.com>
2021-08-27 18:07:40 -0400

    some dependencies and links fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Web3 from 'web3'
 import './App.css';
-import MemoryToken from '../abis/MemoryToken.json'
+import GameToken from '../abis/GameToken.json'
 
 const App = () => {
   return(

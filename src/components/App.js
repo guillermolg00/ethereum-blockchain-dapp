0b815400d2f2122c22861f8a285907a9114d40e5
guillermolg <guillermolopezg00@gmail.com>
2021-08-27 17:42:49 -0400

    blank page with its dependencies

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Web3 from 'web3'
+import './App.css';
+import MemoryToken from '../abis/MemoryToken.json'
+
+const App = () => {
+  return(
+    <div className="">BlockChain App</div>
+  )
+}
+
+export default App
